4b3d71a1b43eca65f9e2fc651c003809cbfb4fad
Raghu Saxena <poiasdpoiasd@live.com>
2020-04-14 03:36:06 +0200

    Listen on localhost

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/v1', api_v1);
 
-http.listen(3000, function(){
+http.listen(3000, '127.0.0.1', function(){
     console.log("Server started on port 3000");
 });
